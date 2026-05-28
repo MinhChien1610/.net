89c6e26f91701c77925807d03f99932d01cf8147
Lê Thị Minh Chiến <lethiminhchien07@gmail.com>
2026-05-28 09:38:48 +0700

    Add files via upload
    Upload file Tuan_14

---
+++ Tuan_13/Tuan_13/View/bai_1.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using Tuan_13.Report;
+
+namespace Tuan_13.View
+{
+    /// <summary>
+    /// Interaction logic for bai_1.xaml
+    /// </summary>
+    public partial class bai_1 : Window
+    {
+        public bai_1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DanhsachSVtheoLop rpt = new DanhsachSVtheoLop();
+            myreport.ViewerCore.ReportSource = rpt;
+        }
+    }
+}
---
+++ Tuan_13/Tuan_13/View/bai_2.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using Tuan_13.Report;
+
+namespace Tuan_13.View
+{
+    /// <summary>
+    /// Interaction logic for bai_2.xaml
+    /// </summary>
+    public partial class bai_2 : Window
+    {
+        public bai_2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MyReport rpt = new MyReport();
+            myreport.ViewerCore.ReportSource = rpt;
+        }
+    }
+}
