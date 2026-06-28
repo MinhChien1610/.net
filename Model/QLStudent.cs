c23e288878a010b95356b21f0cac039604e192bf
Lê Thị Minh Chiến <lethiminhchien07@gmail.com>
2026-06-28 19:10:50 +0700

    Model for tuan_05

---
+++ Model/QLStudent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuan_05.Model
+{
+    namespace Tuan_05.Model
+    {
+        public class QLStudent
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+    }
+}
---
+++ Model/TaskItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuan_05.Model
+{
+    public class TaskItem
+    {
+        public string Name { get; set; }
+        public string Priority { get; set; }
+        public bool IsCompleted { get; set; }
+        public string Note { get; set; }
+    }
+}
