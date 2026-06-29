48d3639839bbd6822a1be42c53dd09e7d11e8695
Lê Thị Minh Chiến <lethiminhchien07@gmail.com>
2026-06-29 20:53:55 +0700

    Tuan_6 View

---
+++ QL_NV_PB.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Tuan_6.ViewModels;
+
+namespace Tuan_6.View
+{
+    /// <summary>
+    /// Interaction logic for QL_NV_PB.xaml
+    /// </summary>
+    public partial class QL_NV_PB : Window
+    {
+        public QL_NV_PB()
+        {
+            InitializeComponent();
+            DataContext = new QL_NV_PBViewModel();
+        }
+    }
+}
---
+++ QuanLiTKNH.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Tuan_6.ViewModels;
+
+namespace Tuan_6.View
+{
+    /// <summary>
+    /// Interaction logic for QuanLiTKNH.xaml
+    /// </summary>
+    public partial class QuanLiTKNH : Window
+    {
+        public QuanLiTKNH()
+        {
+            InitializeComponent();
+            DataContext = new QuanLiTKNHViewModel();
+        }
+    }
+}
