eebe0598d8379bc161fc9233d79535362ad6b5e5
Lê Thị Minh Chiến <lethiminhchien07@gmail.com>
2026-06-30 22:32:08 +0700

    Upload Tuan_07

---
+++ KhaoSatKH.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Tuan_7.ViewModels;
+
+namespace Tuan_7.Views
+{
+    /// <summary>
+    /// Interaction logic for KhaoSatKH.xaml
+    /// </summary>
+    public partial class KhaoSatKH : Window
+    {
+        public KhaoSatKH()
+        {
+            InitializeComponent();
+            DataContext = new KhaoSatKHViewModel();
+        }
+    }
+}
---
+++ UcThongTinKH.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tuan_7.Views
+{
+    /// <summary>
+    /// Interaction logic for UcThongTInKH.xaml
+    /// </summary>
+    public partial class UcThongTinKH : UserControl
+    {
+        public UcThongTinKH()
+        {
+            InitializeComponent();
+        }
+    }
+}
